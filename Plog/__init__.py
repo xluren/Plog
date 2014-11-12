e24ff64b2693fe3b082b8aeb542dc54dc4d7b702
xluren <xluren@126.com>
2014-11-12 11:27:35 +0800

    rewrite readme & note

--- Plog/__init__.py
+++ Plog/__init__.py
@@ -13,13 +13,36 @@ def init_log_conf(log_config_option):
         level=int(logging_level),
         filename=logging_filename
     )
-    logging.warning("hello")
+    logging.info("init log  conf done ")
 
 def transform_datetime(datetime_str,datetime_format):
     unix_time=datetime.datetime.strptime(datetime_str,datetime_format).timetuple()
     return time.mktime(unix_time)
 
 def sink_deal_coroutine(sink_module,sink_option_dict,dict_queue):
+    '''
+        overread_list to store the data we overread
+        eg:
+            the queue has the following data 1,2,3,4,5,6,7
+            we just want to read the data le 5,
+            so when we get 6 from queue,we stop ,but 6 belongs to the data we overread,
+            I store it into overread_list,and at the next time ,when we consume queue,
+            we will read consume the overread_list fist
+        also ,how to judge we have to stop consuming the queue? when we read more than 
+        10 items that we should not read.
+        eg:
+            the queue has the following data 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+            we want to read the number which is less than 2,
+            at the first time ,we got 1,that is what we want ,
+            at the second time ,we got 2,that isn't we want 
+            .
+            .
+            .
+            at the eleven time ,we got 11,that isn't we want 
+            at the same time ,we overread 10 numbers which i dont want ,so we should stop,
+            also,the overread numbers are stored in overread_list.at the next ,we should deali
+            the overread_list first.
+    '''
     overread_list=[]
     cur_dealing_time_list=[]
 
@@ -60,6 +83,7 @@ def sink_deal_coroutine(sink_module,sink_option_dict,dict_queue):
         logging.info("cur_dealing_time_list length is %d " % len(cur_dealing_time_list))
         del cur_dealing_time_list[:]
         #sink_module.deal_sink(dict_list)
+
 def consume_queue_timer(sink_module,sink_option_dict,dict_queue):
 
     interval=int(sink_option_dict["interval"])
@@ -79,6 +103,7 @@ def consume_queue_timer(sink_module,sink_option_dict,dict_queue):
             
 
 def start_work(conf_file):
+
     option_dict=read_conf.get_option_dict(conf_file)
     log_config_option=option_dict["log_config"]
     init_log_conf(log_config_option=log_config_option)
