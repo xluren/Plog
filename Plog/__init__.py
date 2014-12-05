8d19788366c2cd265ff80876e8c95768654437c2
xluren <xluren@126.com>
2014-12-05 11:06:59 +0800

    rewrite some import logical control

--- Plog/__init__.py
+++ Plog/__init__.py
@@ -39,97 +39,24 @@ def init_log_conf(log_config_option):
     )
     logging.info("init log  conf done ")
 
-def transform_datetime(datetime_str,datetime_format):
-    unix_time=datetime.datetime.strptime(datetime_str,datetime_format).timetuple()
-    return time.mktime(unix_time)
-
-def sink_deal_coroutine(sink_module,sink_option_dict,dict_queue):
-    '''
-        overread_list to store the data we overread
-        eg:
-            the queue has the following data 1,2,3,4,5,6,7
-            we just want to read the data le 5,
-            so when we get 6 from queue,we stop ,but 6 belongs to the data we overread,
-            I store it into overread_list,and at the next time ,when we consume queue,
-            we will read consume the overread_list fist
-        also ,how to judge we have to stop consuming the queue? when we read more than 
-        10 items that we should not read.
-        eg:
-            the queue has the following data 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-            we want to read the number which is less than 2,
-            at the first time ,we got 1,that is what we want ,
-            at the second time ,we got 2,that isn't we want 
-            .
-            .
-            .
-            at the eleven time ,we got 11,that isn't we want 
-            at the same time ,we overread 10 numbers which i dont want ,so we should stop,
-            also,the overread numbers are stored in overread_list.at the next ,we should deali
-            the overread_list first.
-    '''
-    overread_list=[]
-    cur_dealing_time_list=[]
-
-    datetime_format=sink_option_dict["datetime_format"]
-    interval=int(sink_option_dict["interval"])
-
-    while 1:
-        dealing_time=(yield)
-        while 1:
-            if len(overread_list)<=0:
-                break
-            dict_item=overread_list[0]
-            transformed_datetime=transform_datetime(
-                datetime_str=dict_item["date_time"],
-                datetime_format=datetime_format
-            )
-            if transformed_datetime<dealing_time+interval:
-                del overread_list[0]
-            elif transformed_datetime>=dealing_time+interval:
-                cur_dealing_time_list.append(dict_item)
-            elif transformed_datetime<dealing_time:
-                del overread_list[0]  
-        dict_queue_size=dict_queue.qsize()
-        while dict_queue.qsize()>0:
-            if len(overread_list)>=10:
-                break
-            dict_item=dict_queue.get()
-            transformed_datetime=transform_datetime(
-                datetime_str=dict_item["date_time"],
-                datetime_format=datetime_format
-            )
-            if transformed_datetime<dealing_time+interval:
-                cur_dealing_time_list.append(dict_item)
-            elif transformed_datetime>=dealing_time+interval:
-                overread_list.append(dict_item)
-            elif transformed_datetime<dealing_time:
-                pass  
-        logging.info("cur_dealing_time_list length is %d " % len(cur_dealing_time_list))
-
-        sink_module.deal_sink(
-                dict_list=cur_dealing_time_list,
-                sink_option_dict=sink_option_dict)
-        
-        del cur_dealing_time_list[:]
-        #sink_module.deal_sink(dict_list)
 
 def consume_queue_timer(sink_module,sink_option_dict,dict_queue):
 
     interval=int(sink_option_dict["interval"])
-    sink_deal_module=sink_deal_coroutine(sink_module,sink_option_dict,dict_queue)
-    sink_deal_module.next()
-
     current_time=time.time()
     dealing_time=current_time-current_time%interval
+    calculate_dict_item=sink_module.deal_sink(sink_option_dict=sink_option_dict)
     while 1:
         current_time=time.time()
-        if dealing_time+interval > current_time:
-            time.sleep(dealing_time+interval-current_time)
-        else :
-            current_time=time.time()
-            sink_deal_module.send(dealing_time)
+
+        if current_time>dealing_time+interval:
             dealing_time=current_time-current_time%interval
-            
+            calculate_dict_item(dict_item="",timeout=1)
+        else :
+            dict_queue_size=dict_queue.qsize()
+            if dict_queue.qsize()>0:
+                dict_item=dict_queue.get()
+                calculate_dict_item(dict_item=dict_item,timeout=0)
 
 def start_work(conf_file):
 
--- Plog/sink/cacheL2get_monitor.py
+++ Plog/sink/cacheL2get_monitor.py
@@ -1,7 +1,7 @@
 import logging
 import platform
 import commands
-def deal_sink(dict_list,sink_option_dict):
+def deal_sink(sink_option_dict):
 
     sink_zabbix_monitor_keys=sink_option_dict['sink_zabbix_monitor_keys']
     service=sink_option_dict['sink_zabbix_monitor_service']
@@ -13,26 +13,27 @@ def deal_sink(dict_list,sink_option_dict):
     for key in sum_keys:
         sum_dict[str(key)]=0
     
-    total_count=0
+    total_count=[0]
 
-    for item in  dict_list:
-        total_count+=1
-        TCP_status=item['TCP_status']
+    def deal_dict_item(dict_item,timeout):
 
-        #the key like TCP_MISS
-        try:
-            sum_dict[str(TCP_status)]+=1
-        except:
-            pass
+        if timeout ==0:
+            total_count[0]+=1
+            #your code here to  calculate or do other thing 
 
-    hostname=platform.uname()[1]
+        else:
+            #your code here 
+            logging.info(total_count[0])
+            total_count[0]=0
+            hostname=platform.uname()[1]
 
-    with open(sink_zabbix_send_file,"w") as file_handle:
-        for key in  sum_dict:
-            info="%s %s%s %d\n" % (hostname,service,str(key),sum_dict[key])
-            file_handle.write(info)
+            with open(sink_zabbix_send_file,"w") as file_handle:
+                for key in  sum_dict:
+                    info="%s %s%s %d\n" % (hostname,service,str(key),sum_dict[key])
+                    file_handle.write(info)
 
-    zabbix_send_cmd="%s %s" % (sink_zabbix_send_cmd,sink_zabbix_send_file)
-    status,output=commands.getstatusoutput(zabbix_send_cmd)
-    logging.info("%d--%s" %(status,output))
+            zabbix_send_cmd="%s %s" % (sink_zabbix_send_cmd,sink_zabbix_send_file)
+            status,output=commands.getstatusoutput(zabbix_send_cmd)
+            logging.info("%d--%s" %(status,output))
 
+    return  deal_dict_item
